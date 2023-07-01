8ddf0bdda0f63a97086d8156779765067989aaa5
akvinayaktiwari <64476491+akvinayaktiwari@users.noreply.github.com>
2023-07-01 23:18:33 +0530

    Update api.js

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { API_NOTIFICATION_MESSEGE ,SERVICE_URL} from '../constants/config';
 import { getAccessToken,getType } from '../utils/common-utils';
 
-const API_URL= 'http://localhost:8000';
+const API_URL= 'blog-application-server-nine.vercel.app';
 
 const axiosInstance=axios.create({
     baseURL:API_URL,
