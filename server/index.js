815140f9855acff8a16b911726b957a226de7c47
akvinayaktiwari <64476491+akvinayaktiwari@users.noreply.github.com>
2023-07-01 23:25:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app=express();
 
 app.use(cors(
     {
-    origin: ["https://deploy-mern-lwhq.versel.app"],
+    origin: ["https://blog-application-client.vercel.app/"],
     methods:["POST","GET","PUT","DELETE"],
     credentials: true
     }
