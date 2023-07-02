4f6864cf632effec9f1211a4a3923ca9d0a5fca3
akvinayaktiwari <64476491+akvinayaktiwari@users.noreply.github.com>
2023-07-02 12:27:00 +0530

    Update index.js vercel

--- server/index.js
+++ server/index.js
@@ -9,7 +9,13 @@ import Router from './routes/route.js'
 dotenv.config();
 
 const app=express();
-
+if (process.env.NODE_ENV == "production") {
+  const path = require("path");
+  app.use(express.static(path.resolve(__dirname, "client", "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 app.use(cors());
 app.use(bodyParser.json({extended:true}))
 app.use(bodyParser.urlencoded({extended:true}))
