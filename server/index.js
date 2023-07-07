6dee7f44a5b3d3764591823e926670f526a99f7a
akvinayaktiwari <64476491+akvinayaktiwari@users.noreply.github.com>
2023-07-07 23:27:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,13 +16,7 @@ app.use(bodyParser.json({extended:true}))
 app.use(bodyParser.urlencoded({extended:true}))
 app.use('/',Router)
 
-if (process.env.NODE_ENV == "production") {
-  const path = require("path");
-  app.use(express.static(path.resolve(__dirname, "client", "build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+
 
 
 const PORT=process.env.PORT||8000;
