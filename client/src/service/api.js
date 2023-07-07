a3e0bb0197141b5a86599ff55fd74d46886da34c
akvinayaktiwari <64476491+akvinayaktiwari@users.noreply.github.com>
2023-07-07 23:31:29 +0530

    Update api.js final

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { API_NOTIFICATION_MESSEGE ,SERVICE_URL} from '../constants/config';
 import { getAccessToken,getType } from '../utils/common-utils';
 
-const API_URL= 'http://localhost:8000';
+const API_URL= 'https://blog-application-server-je65.onrender.com';
 
 const axiosInstance=axios.create({
     baseURL:API_URL,
